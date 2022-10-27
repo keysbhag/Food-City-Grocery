53d5b61d817c3535508d3443949be8d6b2ced93a
KennyLingx <112728719+KennyLingx@users.noreply.github.com>
2022-10-26 23:38:19 -0400

    makelogin logoutjs and seeds

---
+++ seeds/seed.js
@@ -0,0 +1,24 @@
+const sequelize = require('../config/connection');
+const { User, Product, Category, Cart } = require('../models');
+
+const userData = require('./userData.json');
+const productData = require('./productData.json');
+const categoryData = require('./categoryData.json');
+const cartData = require('./cartData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+    
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+    await Category.bulkCreate(categoryData);
+
+    await Product.bulkCreate(productData);
+
+    await Cart.bulkCreate(cartData);
+}
+
+
+seedDatabase();
