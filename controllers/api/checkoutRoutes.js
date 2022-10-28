79d47ee83668ee8852a05cea44579b96b080556e
keyshawn bhagwandin <keyshawn.bhag@hotmail.com>
2022-10-28 10:31:50 -0400

    fixed checkout route

--- controllers/api/checkoutRoutes.js
+++ controllers/api/checkoutRoutes.js
@@ -6,7 +6,7 @@ let date = new Date();
 let month = date.getMonth() + 1;
 let year = date.getFullYear();
 
-router.post("/", async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   try {
     let validCard = luhn.validate(req.body.creditCard);
     let validMonth = req.body.month > month;
@@ -16,7 +16,7 @@ router.post("/", async (req, res) => {
     if (validCard && validMonth && validYear) {
       const newItem = await Cart.destroy({
         where: {
-          user_id: 1,
+          user_id: req.session.user_id,
         },
       });
       res.status(200).json(`You have successfully cashed out!`+newItem);
