82a2f8ce873f96fc816977a3b2e8c33b920316cc
keyshawn bhagwandin <keyshawn.bhag@hotmail.com>
2022-10-28 10:15:58 -0400

    adjusted handlebar

--- public/js/login.js
+++ public/js/login.js
@@ -22,10 +22,10 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#name-signup').value.trim();
+    const name = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-  
+    
     if (name && email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
@@ -34,7 +34,7 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/profile');  //change the endpoint
+        document.location.replace('/');  //change the endpoint
       } else {
         alert(response.statusText);
       }
