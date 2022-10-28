b55b8b9cdbf6749a33f68745e0ce39de07980cf8
keyshawn bhagwandin <keyshawn.bhag@hotmail.com>
2022-10-28 12:33:19 -0400

    added success route

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const successRoutes = require('./successRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/success', successRoutes);
 
 module.exports = router;
