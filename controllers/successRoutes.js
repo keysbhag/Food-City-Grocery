40c42c22f997aecdaf9ecde947c2fb900e04288e
keyshawn bhagwandin <keyshawn.bhag@hotmail.com>
2022-10-28 14:17:19 -0400

    made success page and helpers

--- controllers/successRoutes.js
+++ controllers/successRoutes.js
@@ -3,4 +3,30 @@ const { User, Category, Product, Cart } = require("../models");
 const withAuth = require("../utils/auth");
 
 
+router.get("/", async (req, res) => {
+  try {
+    const orderData = await User.findByPk(req.session.user_id || 2, {
+      include: [
+        {
+          model: Product,
+          through: {
+            model: Cart,
+          },
+        },
+      ],
+    });
+
+    // res.status(200).json(orderData);
+    
+    const order = orderData.get({ plain: true });
+
+    res.render("success", {
+      ...order,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
--- utils/helper.js
+++ utils/helper.js
@@ -7,4 +7,15 @@ module.exports = {
     format_amount: (amount) => {
       return parseInt(amount).toLocaleString();
     },
+    multiply_price: (price, quantity) => {
+      return price * quantity;
+    },
+    find_sum: (products) => {
+      let sum = 0;
+      for (let i = 0; i < products.length; i++){
+        let bag = products[i].price * products[i].cart.quantity;
+        sum = sum + bag;
+      }
+      return sum
+    }
 }
