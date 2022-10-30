393f71ec6debedb6a5f65bd08ff04c80f5088705
keyshawn bhagwandin <keyshawn.bhag@hotmail.com>
2022-10-30 14:23:13 -0400

    added better credit card checker

--- controllers/api/checkoutRoutes.js
+++ controllers/api/checkoutRoutes.js
@@ -10,16 +10,32 @@ router.post("/", withAuth, async (req, res) => {
   try {
     let validCard = luhn.validate(req.body.creditCard);
     let validMonth = req.body.month > month;
-    let validYear = req.body.year > year;
+    let validYear = req.body.year >= year;
     console.log(`${validCard},${validMonth},${validYear}`)
-
-    if (validCard && validMonth && validYear) {
-      const newItem = await Cart.destroy({
-        where: {
-          user_id: req.session.user_id,
-        },
-      });
-      res.status(200).json(`You have successfully cashed out ${newItem}!`);
+    
+    if(req.body.year == year){
+      if (validCard && validMonth && validYear) {
+        const newItem = await Cart.destroy({
+          where: {
+            user_id: req.session.user_id,
+          },
+        });
+        res.status(200).json(`You have successfully cashed out ${newItem}!`);
+      } else {
+      res.status(404).json({ message: `entered card info is invalid! ${err}`});
+      }
+    }     
+    else if(req.body.year > year){
+      if (validCard && validYear) {
+        const newItem = await Cart.destroy({
+          where: {
+            user_id: req.session.user_id,
+          },
+        });
+        res.status(200).json(`You have successfully cashed out ${newItem}!`);
+      } else {
+      res.status(404).json({ message: `entered card info is invalid! ${err}`});
+      }
     } else {
       res.status(404).json({ message: `entered card info is invalid! ${err}`});
     }
