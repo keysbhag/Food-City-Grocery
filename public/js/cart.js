a850fe0b0483e6e51e1da3a0810f2c5861771c4c
keyshawn bhagwandin <keyshawn.bhag@hotmail.com>
2022-10-30 10:46:39 -0400

    made a function to the stock of the item

--- public/js/cart.js
+++ public/js/cart.js
@@ -3,10 +3,18 @@ const updateItemHandler = async (event) => {
     const id = event.target.getAttribute("data-id");
     const product_id = event.target.getAttribute("data-descr");
     const quantity = document.querySelector('#c'+id).value;
+    const stock = event.target.getAttribute("data-val");
+    const origQuan = event.target.getAttribute("data-orig");
+    let newStock = 0;
+    if (quantity > origQuan) {
+      newStock = parseInt(stock) - parseInt(quantity - origQuan);
+    } else {
+      newStock = parseInt(stock) + parseInt(origQuan - quantity);
+    }
 
     const response = await fetch(`/api/cart/${id}`, {
       method: "PUT",
-      body: JSON.stringify({ product_id, quantity }),
+      body: JSON.stringify({ product_id, quantity, newStock }),
       headers: {
         "Content-Type": "application/json",
       },
@@ -24,9 +32,17 @@ const updateItemHandler = async (event) => {
 const delItemHandler = async (event) => {
   if (event.target.hasAttribute("data-id")) {
     const id = event.target.getAttribute("data-id");
+    const product_id = event.target.getAttribute("data-descr");
+    const stock = event.target.getAttribute("data-val");
+    const quantity = event.target.getAttribute("data-orig");
+    const newStock = parseInt(stock) + parseInt(quantity);
 
     const response = await fetch(`/api/cart/${id}`, {
       method: "DELETE",
+      body: JSON.stringify({ product_id, newStock }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
 
     if (response.ok) {
